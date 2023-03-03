dded9497a9b95caeeb6f6bae63c208fe356b8800
Guigssss <guillaume.mouty@gmail.com>
2023-03-03 19:23:52 +0100

    changing app.py problems

--- app.py
+++ app.py
@@ -1,37 +1,34 @@
-from flask import Flask, render_template, request, session, jsonify
-
+from flask import Flask, request, render_template
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('db', 27017)
+client = MongoClient("mongodb://mongodb:27017/")
 db = client.todoapp
 
 
-@app.route('/', methods=['GET', 'POST'])
-def form():
-    if request.method == 'POST':
-        todos = []
-        for todo in db.todos.find():
-            todos.append({
-                'title': todo['title']
-            })
-
-        title = request.json['name']
-        todo = {
-            'title': title,
-        }
-        result = db.todos.insert_one(todo)
-        todos.append(todo)
-        return render_template('form.html', names=todos)
-    else:
-        todos = []
-        for todo in db.todos.find():
-            todos.append({
-                'title': todo['title']
-            })
-        return render_template('form.html', names=todos)
+@app.route('/', methods=['GET'])
+def get_todos():
+    todos = []
+    for todo in db.todos.find():
+        todos.append(todo['title'])
+    return render_template('form.html', names=todos)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0",port = 5001)
+@app.route('/', methods=['POST'])
+def create_todo():
+    todos = []
+    for todo in db.todos.find():
+        todos.append({
+            'title': todo['title']
+        })
+    title = request.form['name']
+    todo = {
+        'title': title,
+    }
+    result = db.todos.insert_one(todo)
+    todos.append(todo)
+    return render_template('form.html', names=todos)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)
