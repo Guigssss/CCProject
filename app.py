6e590cfdee97a8cb5215b386bfd3dfeaaa92f6af
Guigssss <guillaume.mouty@gmail.com>
2023-03-06 15:40:48 +0100

    changing app.py problems and adding footer

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, url_for
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient("127.0.0.1",27017)
+client = MongoClient("mongodb://mongodb:27017/")
 db = client.todoapp
 
 
 @app.route('/', methods=['GET'])
 def get_todos():
+    with open('data/footer.txt') as file:
+        footer = file.read()
     todos = []
     for todo in db.todos.find():
         todos.append(todo['title'])
-    return render_template('form.html', names=todos)
+    return render_template('form.html', names=todos, footer=footer)
 
 
 @app.route('/', methods=['POST'])
@@ -26,12 +28,13 @@ def create_todo():
     }
     result = db.todos.insert_one(todo)
     todos.append(title)
-    return render_template('form.html', names=todos)
+    return redirect(url_for('get_todos'))
+
 
 @app.route('/clear', methods=['POST'])
 def execute():
     db.todos.delete_many({})
-    return render_template('form.html')
+    return redirect(url_for('get_todos'))
 
 
 if __name__ == '__main__':
